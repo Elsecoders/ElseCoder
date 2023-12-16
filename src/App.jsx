1fe32a98829e27db246842273622e08f825b48cf
Muhammad Jawwad <jawwadjunaid17@gmail.com>
2023-12-16 17:36:12 +0500

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 import Navs from '../Components/Navbar/Navs';
@@ -27,29 +27,8 @@ import Footer from '../Components/footer/Footer';
   
   export default App;
   
-=======
-import "bootstrap/dist/css/bootstrap.min.css";
-import Navs from "../Components/Navbar/Navs";
-import SliderSection from "../Components/body/SliderSection/SliderSection";
-import Introduction from "../Components/body/Introduction/Introduction";
-import Agency from "../Components/body/Agency/Agency";
-import MiddleContent from "../Components/body/MiddleContent/MiddleContent";
-import ServicesWeOffer from "../Components/body/ServicesWeOffer/ServicesWeOffer";
-import InformationalBolg from "../Components/body/InformationalBlog/InformationalBolg";
 
-const App = () => {
-  return (
-    <div>
-      <Navs />
-      <SliderSection />
-      <Introduction />
-      <Agency />
-      <MiddleContent />
-      <ServicesWeOffer />
-      <InformationalBolg />
-    </div>
-  );
-};
 
-export default App;
->>>>>>> 2cbc2d6a3a84e1a252b65dc707a49a2ae56eed83
+
+
+

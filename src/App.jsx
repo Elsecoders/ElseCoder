3ea824ea2ccd7b3b7ec3ba2140d6a338dd87efca
Muhammad Jawwad <jawwadjunaid17@gmail.com>
2023-12-19 17:56:17 +0500

    update footer and model

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'; 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Aboutus from './Pages/AboutUs/Aboutus';
 import MainBody from '../Components/MainBody/MainBody';
 import Services from './Pages/ServicesE/Services';
@@ -13,6 +13,7 @@ import Contactus from './Pages/ContactUs/Contactus';
       <>
       <BrowserRouter>
       <Routes>
+      <Route path="*" element={<Navigate to="/home" />} />
         <Route path='/home' element={<MainBody/>}/>
         <Route path='/aboutus' element={<Aboutus/>}/>
         <Route path='/services' element={<Services/>}/>

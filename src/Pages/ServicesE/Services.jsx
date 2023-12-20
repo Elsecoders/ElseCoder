9ef4c32c062df444c982463d6dd8548e90406298
Muhammad Jawwad <jawwadjunaid17@gmail.com>
2023-12-21 03:50:06 +0500

    jawwad-dev

--- src/Pages/ServicesE/Services.jsx
+++ src/Pages/ServicesE/Services.jsx
@@ -1,12 +1,153 @@
 import React from "react";
 import Navs from "../../../Components/Navbar/Navs";
 import Footer from "../../../Components/footer/Footer";
-
+import "./Services.css";
+import { Link } from "react-router-dom";
+import IntegrationInstructionsIcon from '@mui/icons-material/IntegrationInstructions';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import CodeIcon from '@mui/icons-material/Code';
+import NorthEastIcon from '@mui/icons-material/NorthEast';
 const Services = () => {
   return (
     <>
       <Navs />
-      <Footer />
+      <div className="servicePage mt-4">
+        <div className="Services">
+          <div className="ServiceImg">
+            <img src="/Images/SliderImg/slider.jpg" alt="Slider" />
+          </div>
+          <div className="Service-content">
+            <h1 style={{ color: "black" }}>We Engineer Software Solutions</h1>
+            <p>
+              We help you envision technology that powers the future of your
+              business
+            </p>
+          </div>
+        </div>
+        <div className="whatWedo">
+          <h2 style={{ color: "black", textAlign: "center" }}>What We Do</h2>
+          <div className="solution mt-5">
+            <ul
+              className="sol-link"
+              style={{
+                gap: "20px",
+                display: "flex",
+                justifyContent: "space-between",
+                listStyleType: "none",
+              }}
+            >
+              <Link
+                className="link-with-line"
+                style={{ textDecoration: "none", color: "#777" }}
+              >
+                Solution Implementation<span className="line"></span>
+              </Link>
+              <Link
+                className="link-with-line"
+                style={{ textDecoration: "none", color: "#777" }}
+              >
+                Software Development<span className="line"></span>
+              </Link>
+              <Link
+                className="link-with-line"
+                style={{ textDecoration: "none", color: "#777" }}
+              >
+                Search Engine Optimization<span className="line"></span>
+              </Link>
+              <Link
+                className="link-with-line"
+                style={{ textDecoration: "none", color: "#777" }}
+              >
+                Social Media Management<span className="line"></span>
+              </Link>
+            </ul>
+          </div>
+        </div>
+        <div className="service-div mt-5">
+          <div className="service-div-1">
+            <div className="service-div-content">
+              <h1>
+                <span>Solution</span>
+                <br></br>
+                <span style={{ color: "#2CA3CC" }}>Implementation</span>
+              </h1>
+              <p>
+                As a trusted partner of world's leading CRM solutions, <br></br>we can
+                help you customize and implement business solutions 3x faster
+                and smarter.
+              </p>
+            </div>
+            <div className="service-div-sol">
+              <box className="box-1"><IntegrationInstructionsIcon/>Word Press<NorthEastIcon/></box><br></br>
+              <box className="box-1"><CodeIcon/>Custom React CMS<NorthEastIcon/></box>
+              <box className="box-1"><ShoppingCartIcon/>Shopify<NorthEastIcon/></box>
+            </div>
+          </div>
+          <div className="service-div-1">
+            <div className="service-div-content">
+              <h1>
+                <span>Solution</span>
+                <br></br>
+                <span style={{ color: "#2CA3CC" }}>Implementation</span>
+              </h1>
+              <p>
+                As a trusted partner of world's leading CRM solutions, <br></br>we can
+                help you customize and implement business solutions 3x faster
+                and smarter.
+              </p>
+            </div>
+            <div className="service-div-sol">
+              <box className="box-1"><IntegrationInstructionsIcon/>Word Press<NorthEastIcon/></box><br></br>
+              <box className="box-1"><CodeIcon/>Custom React CMS<NorthEastIcon/></box>
+              <box className="box-1"><ShoppingCartIcon/>Shopify<NorthEastIcon/></box>
+            </div>
+          </div>
+          <div className="service-div-1">
+            <div className="service-div-content">
+              <h1>
+                <span>Solution</span>
+                <br></br>
+                <span style={{ color: "#2CA3CC" }}>Implementation</span>
+              </h1>
+              <p>
+                As a trusted partner of world's leading CRM solutions, <br></br>we can
+                help you customize and implement business solutions 3x faster
+                and smarter.
+              </p>
+            </div>
+            <div className="service-div-sol" style={{justifyContent:"space-between"}}>
+              <box className="box-1"><IntegrationInstructionsIcon/>Word Press<NorthEastIcon/></box><br></br>
+              <box className="box-1"><CodeIcon/>Custom React CMS<NorthEastIcon/></box>
+              <box className="box-1"><ShoppingCartIcon/>Shopify<NorthEastIcon/></box>
+            </div>
+          </div>
+          <div className="service-div-1">
+            <div className="service-div-content">
+              <h1>
+                <span>Solution</span>
+                <br></br>
+                <span style={{ color: "#2CA3CC" }}>Implementation</span>
+              </h1>
+              <p>
+                As a trusted partner of world's leading CRM solutions, <br></br>we can
+                help you customize and implement business solutions 3x faster
+                and smarter.
+              </p>
+            </div>
+            <div className="service-div-sol">
+              <box className="box-1"><IntegrationInstructionsIcon/>Word Press<NorthEastIcon/></box><br></br>
+              <box className="box-1"><CodeIcon/>Custom React CMS<NorthEastIcon/></box>
+              <box className="box-1"><ShoppingCartIcon/>Shopify<NorthEastIcon/></box>
+            </div>
+          </div>
+        </div>
+      </div>
+    
+     
+     
+    
+    
+    <Footer/>
     </>
   );
 };
